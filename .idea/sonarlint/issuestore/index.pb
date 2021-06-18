f603bdb9a18aaac5a1c6d0202536e36d95b3ead8
rajatsangrame <rajatsangrame@gmail.com>
2021-06-18 16:16:57 +0530

    added saved state

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-m
-=app/src/main/java/com/example/myapplication/ComponentUtils.kt,2/9/297d8c0245435e4ffa9c27e1eae4c541e5d79f9e
->
-app/.gitignore,5/1/51e1c5d383dfaa35e0e7e5873a0a99355a86880f
--- app/src/main/java/com/example/myapplication/RPKSelectButton.kt
+++ app/src/main/java/com/example/myapplication/RPKSelectButton.kt
@@ -2,11 +2,12 @@ package com.example.myapplication
 
 import android.R.attr
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
-import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
@@ -16,17 +17,24 @@ import androidx.core.content.res.ResourcesCompat
 import kotlin.math.abs
 
 /**
- * Ref: https://medium.com/android-news/perfmatters-introduction-to-custom-viewgroups-to-improve-performance-part-2-f14fbcd47c
+ * Ref:
+ * https://medium.com/android-news/perfmatters-introduction-to-custom-viewgroups-to-improve-performance-part-2-f14fbcd47c
+ * https://stackoverflow.com/questions/59629541/how-to-save-fragment-state-while-navigating-with-navigation-component
+ * https://medium.com/super-declarative/android-how-to-save-state-in-a-custom-view-30e5792c584b
+ * https://github.com/DonBrody/Android-ResizableRelativeLayout/blob/master/app/src/main/java/com/donbrody/resizablerelativelayout/components/ResizableRelativeLayout.kt
  */
 class RPKSelectButton : FrameLayout {
 
   private var font = ResourcesCompat.getFont(context, R.font.gilroy_medium)
-  private var textSize = 16f
+  private val textSize = 16f
   private val textPadding: Int = 12.toPx
   private lateinit var textView: TextView
   private lateinit var imageView: ImageView
   private var labelText: String? = null
-  private val TAG = "RPKSelectButton"
+
+  companion object {
+    private const val TAG = "RPKSelectButton"
+  }
 
   constructor(context: Context, attr: AttributeSet?) : super(context, attr) {
     attr?.let {
@@ -34,6 +42,7 @@ class RPKSelectButton : FrameLayout {
     }
     setupBackground()
     setupUi()
+    unselect()
   }
 
   private fun obtainAttrValues(attr: AttributeSet) {
@@ -43,13 +52,14 @@ class RPKSelectButton : FrameLayout {
   }
 
   private fun setupUi() {
+    Log.d(TAG, "setupUi: isButtonSelected ${this.getIsSelected()}")
     setupImageView()
     setupTextView()
-    unselect()
   }
 
   private fun setupTextView() {
     textView = TextView(context)
+    textView.id = generateViewId()
     val tvParams = LayoutParams(
       ViewGroup.LayoutParams.WRAP_CONTENT,
       ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -65,6 +75,7 @@ class RPKSelectButton : FrameLayout {
 
   private fun setupImageView() {
     imageView = ImageView(context)
+    imageView.id = generateViewId()
     val layoutParams =
       LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER)
     imageView.layoutParams = layoutParams
@@ -105,6 +116,8 @@ class RPKSelectButton : FrameLayout {
     return super.performClick()
   }
 
+  private fun getIsSelected(): Boolean = isSelected
+
   override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
 
     //Log.d(TAG, "onMeasure: left $left top $top right $right bottom $bottom")
@@ -122,6 +135,41 @@ class RPKSelectButton : FrameLayout {
 
     textView.layout(l, t, r, b)
     imageView.layout(0, 0, abs(right - left), abs(top - bottom))
+
+  }
+
+  override fun onSaveInstanceState(): Parcelable {
+    Log.d(TAG, "onSaveInstanceState: ")
+    val superState = super.onSaveInstanceState()
+    val savedState = SavedState(superState)
+    savedState.selected = this.getIsSelected()
+    return savedState
+  }
+
+  override fun onRestoreInstanceState(state: Parcelable?) {
+    val savedState = state as SavedState
+    super.onRestoreInstanceState(savedState.superState)
+    if (savedState.selected) {
+      this.select()
+    } else {
+      this.unselect()
+    }
+    Log.d(TAG, "onRestoreInstanceState: isButtonSelected ${this.getIsSelected()}")
+
+  }
+
+  private inner class SavedState : BaseSavedState {
+    var selected = false
+
+    constructor(parcel: Parcelable?) : super(parcel)
+
+    constructor(parcel: Parcel) : super(parcel) {
+      selected = parcel.readInt() != 0
+    }
+
+    override fun writeToParcel(out: Parcel, flags: Int) {
+      out.writeInt(if (selected) 1 else 0)
+    }
 
   }
 
